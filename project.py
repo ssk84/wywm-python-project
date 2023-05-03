10c8f289c26ced2fefd9f2e74a37c9f7f0a5563d
SatyaS <ssk84@users.noreply.github.com>
2023-05-03 01:15:01 -0400

    Fixed feedback comments

--- project.py
+++ project.py
@@ -1,25 +1,39 @@
+"""Module to generate random numbers"""
 import random
 import json
 
-# Function to create a new knight
+# Function to create a new knight object and save to the list
 def create_knight(knights):
     print("Let's create a knight!")
+    
     user_input_ques = "What is knight's name? "
     # empty string not allowed
-    kname = get_valid_str(user_input_ques)
+    knights_name = get_valid_str(user_input_ques)
     user_input_ques = "What is knight's group? "
-    kgroup = get_valid_str(user_input_ques)
+    knights_group = get_valid_str(user_input_ques)
     user_input_ques = "What is knight's weapon? "
-    kweapon = get_valid_str(user_input_ques)
-    kage = random.randrange(20, 50)
-    kid = random.randint(100, 1000)
+    knights_weapon = get_valid_str(user_input_ques)
+    knights_age = random.randrange(20, 50)
+    
+    # To assure unique knight_id is created
+    while True:
+        knights_id = random.randint(100, 1000)
+        is_exists = find_knight_by_id(knights_id, knights)
+        if is_exists:
+            continue
+        else:
+            break
+    
     print("--- Your Knight ---")
-    k = Knight(kid, kname, kage, kgroup, kweapon)
-    print(json.dumps(k.__dict__))
+    k = Knight(knights_id, knights_name, knights_age, knights_group, knights_weapon)
     knights.append(k)
+    Knight.display_knight(k)
 
-# Call a knight and change their data - name/age/group/weapon
-# selecting option 0 will display the changes and exit function
+
+""" 
+Call a knight and change their data - name/age/group/weapon. 
+Selecting option 0 will display the updated changes to the knight and exit function 
+"""
 def change_data(knight_to_update_by_id, knights):
     while True:
         print("--- What would you like to update? ---")
@@ -28,58 +42,82 @@ def change_data(knight_to_update_by_id, knights):
         print("3.Knight's group")
         print("4.Knight's weapon")
         print("0.Exit")
+        
         user_input_ques = "Select your option [0/1/2/3/4]: "
         select_option = get_valid_menu_option(0, user_input_ques)
-        print("You have a knight!")
+        #print("You have a knight!")
         #print("select_option: "+str(select_option))
-        if int(select_option) == 1:
-            update_item = "name"
-        if int(select_option) == 2:
-            update_item = "age"
-        if int(select_option) == 3:
-            update_item = "group"
-        if int(select_option) == 4:
-            update_item = "weapon"        
-        if int(select_option) != 0:
-            user_input_ques = "What is their new "+update_item+"? "
-            new_item = get_valid_str(user_input_ques)
+        
+        update_item = ''
+        new_item = ''
+        match int(select_option):    
+            case 1:
+                update_item = "name"
+            case 2:
+                update_item = "age"
+            case 3:
+                update_item = "group"
+            case 4:
+                update_item = "weapon"
+            case 0:
+                pass
+        
+        if select_option != 0:
+            user_input_ques = str("What is their new "+update_item+"? ")
+            new_item = get_valid_str(user_input_ques) 
+        
         for i in range(0, len(knights)):
             if(knights[i].id == knight_to_update_by_id):
-                if select_option == 1:
-                    knights[i].name = new_item
-                if select_option == 2:
-                    knights[i].age = new_item
-                if select_option == 3:
-                    knights[i].group = new_item
-                if select_option == 4:
-                    knights[i].weapn = new_item
-                if select_option == 0:
-                    print(json.dumps(knights[i].__dict__))
+                match select_option:
+                    case 0:
+                        Knight.display_knight(knights[i])
+                    case 1:
+                        knights[i].name = new_item
+                    case 2:
+                        knights[i].age = new_item
+                    case 3:
+                        knights[i].group = new_item
+                    case 4:
+                        knights[i].weapon = new_item
+                if select_option != 0:
+                    Knight.display_knight(knights[i])
+        
         if int(select_option) == 0:
-            break
+            break # breaks the while loop
 
-# Show the current knights and select one
+# Displays all the knights in the list, validates knight_id exists and returns it
 def select_knight(knights):
-    print("What knight would you like to update?")
-    print_knights(knights)
+    """If there are no knights, display the msg to create knights list"""
+    if len(knights)<1:
+        print("There are no knights to update. Create knights first.")
+        menu()
+    else:
+        print("What knight would you like to update?")
+        print_knights(knights)
+    
     user_input_ques = "Select the knight's id: "
-    knight_to_update_by_id = find_knight_by_id(user_input_ques, knights)
+    knight_to_update_by_id = get_knight_id_for_update(user_input_ques, knights)
+    
     return knight_to_update_by_id
+
 
 # prints all the knights in json format
 def print_knights(knights):
     for knight in knights:
-        print(json.dumps(knight.__dict__))
+        Knight.display_knight(knight)
 
-# This is the menu to make our selections
-def menu(knights_number):
+
+# This is the main menu to begin the program
+def menu():
     while True:
         print("What do you want to do")
         print("1: Create a new knight")
         print("2: Update your knight")
         print("0: Exit")
+        
         user_input_ques = "Selection number: "
         menu_item = get_valid_menu_option(1, user_input_ques)
+        
         if menu_item == 0:
             print_knights(knights)
             exit(0)
@@ -89,59 +127,88 @@ def menu(knights_number):
             knight_to_update_by_id = select_knight(knights)
             change_data(knight_to_update_by_id, knights)
 
-# Find and return selected knight's id from knights list
-def find_knight_by_id(user_input_ques, knights):
+
+# Get a valid knight_id from user input for change_data function
+def get_knight_id_for_update(user_input_ques, knights):
     is_valid = False
+    
     while not is_valid:
-        user_input_kid = input(user_input_ques)
-        if user_input_kid.isdigit():
-            for knight in knights:
-                if knight.id == int(user_input_kid):
-                    is_valid = True
-                    break
-            if not is_valid:
+        user_input_knight_id = input(user_input_ques)
+        if user_input_knight_id.isdigit():
+            # Check if given knight_id exists in the list or not
+            is_found = find_knight_by_id(user_input_knight_id, knights)
+            if is_found:
+                is_valid = True
+                break
+            else:
                 print("No matching knight's id exists. Please re-enter.")
                 continue
         else:
             print("Invalid knight's id must be number. Please re-enter.")
-        print(user_input_kid)
-    return int(user_input_kid)
+        
+        print(user_input_knight_id)
+    
+    return int(user_input_knight_id)
 
-# Generic function to validate the input menu option
-# If invalid option is entered, user is prompted again to provide input
-# For menu item => item=1, for knight item update => item=0
+
+# Find and return selected knight's id from knights list
+def find_knight_by_id(knight_id, knights):
+    is_found = False
+    for knight in knights:
+        if knight.id == int(knight_id):
+            is_found = True
+            break
+    
+    return is_found
+
+
+""" 
+Generic function to validate the input menu option
+If invalid option is entered, user is prompted again to provide input
+For main menu item use item=1, for knight item update use item=0
+"""
 def get_valid_menu_option(item, user_input_ques):
     is_valid = False
+    
     while not is_valid:
         selected_option = input(user_input_ques)
-        if selected_option.isdigit(): 
-            if int(item) == 1 and (int(selected_option) == 0
-                                  or int(selected_option) == 1
-                                  or int(selected_option) == 2):
-                is_valid = True
-            elif int(item) == 0 and (int(selected_option) == 1
-                                     or int(selected_option) == 2
-                                     or int(selected_option) == 3
-                                     or int(selected_option) == 4
-                                     or int(selected_option) == 0):
-                is_valid = True
+        
+        if selected_option.isdigit():
+            match item:
+                case 0: # menu options for updating knight
+                    if (int(selected_option) == 1
+                        or int(selected_option) == 2
+                        or int(selected_option) == 3
+                        or int(selected_option) == 4
+                        or int(selected_option) == 0):
+                        is_valid = True
+                case 1: # main menu options
+                    if (int(selected_option) == 0
+                        or int(selected_option) == 1
+                        or int(selected_option) == 2):
+                        is_valid = True
         else:
             print("Invalid option selected, please re-enter.")
             continue
+    
     return int(selected_option)
 
+
 # Function to check if non-empty string is provided in the input
-def get_valid_str(askinput):
+def get_valid_str(user_input):
     is_empty = True
+    
     while is_empty:
-        userinput = input(askinput)
-        if userinput != '':
+        valid_str = input(user_input)
+        if valid_str != '':
             is_empty = False
             break
         else:
             print("Input cannot be empty. Please enter a string.")
             continue
-    return userinput
+    
+    return valid_str
+
 
 # Knight class
 class Knight:
@@ -151,14 +218,16 @@ class Knight:
         self.age = age
         self.group = group
         self.weapon = weapon
+    
+    def display_knight(self):
+        print(json.dumps(self.__dict__))
+
 
 # Setting the scene
-knights_number = 0
 knights = []
-options = []
 
 # Run the program
-menu(knights_number)
+menu()
 
 
 
